1a115e7b4feae3bea58bd1e0d0fc9fa6fef37c15
Otto Sipe <ottosipe@gmail.com>
2013-05-18 14:52:48 -0700

    got rid of admin cruft

--- kaffee/urls.py
+++ kaffee/urls.py
@@ -1,18 +1,8 @@
 from django.conf.urls import patterns, include, url
 
-
 from kaffee import views
-# Uncomment the next two lines to enable the admin:
-from django.contrib import admin
-admin.autodiscover()
 
 urlpatterns = patterns('',
-    # Examples:
     url(r'^$', 'kaffee.views.index'),
-    url(r'^data/$', 'kaffee.api.data'),
-
-    # Uncomment the admin/doc line below to enable admin documentation:
-    url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
-    # Uncomment the next line to enable the admin:
-    url(r'^admin/', include(admin.site.urls)),
+    url(r'^data$', 'kaffee.api.data')
 )
